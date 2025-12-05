73278947080246146418db8c3529070ac2e9dad2
Mathis TONG--HATET <mathistonghatet@gmail.com>
2025-12-05 15:30:47 +0100

    changement routeur + debut arbo

--- images_php_sqlite/data/SMIYC/app/Config/Routes.php
+++ images_php_sqlite/data/SMIYC/app/Config/Routes.php
@@ -17,26 +17,38 @@ $routes->get('/', 'Home::index');
 $routes->get('login', 'Auth::login');
 $routes->get('register', 'Auth::register');
 $routes->get('forgot-password', 'Auth::forgotPassword');
+
+
+// ---------------- Profile ----------------
 $routes->get('profile', 'Auth::profile');
-$routes->get('profile/edit', 'Auth::editProfile');
+$routes->get('orders/status/(:num)', 'Orders::status/$1'); // statut d'une commande
+
+//                   TODO
+// suivi commande
+// moyen de payement (carte saved)
+// historique des commandes
+// Langues et regions
+
+// $routes->get('orders/history', 'Orders::history');
+// $routes->get('profile/edit', 'Auth::editProfile');
+
 
 // ---------- Catalogue et produits ----------
 $routes->get('catalogue', 'Catalogue::index');
 $routes->get('product/(:num)', 'Catalogue::detail/$1');
 $routes->get('search', 'Catalogue::search');
 
+
 // ---------- Panier ----------
 $routes->get('cart', 'Cart::index');
-$routes->get('checkout', 'Cart::checkout');
-
+// $routes->get('checkout', 'Cart::checkout');  -> a voir
 // ---------- Paiement ----------
 $routes->get('payment', 'Payment::index');
 $routes->get('payment/success', 'Payment::success');
 $routes->get('payment/error', 'Payment::error');
-
 // ---------- Commandes ----------
-$routes->get('orders/status/(:num)', 'Orders::status/$1'); // statut d'une commande
-$routes->get('orders/history', 'Orders::history');
+
+
 
 // ---------- Administration ----------
 $routes->get('admin/dashboard', 'Admin::dashboard');
@@ -44,6 +56,7 @@ $routes->get('admin/products', 'Admin::products');
 $routes->get('admin/users', 'Admin::users');
 $routes->get('admin/orders', 'Admin::orders');
 $routes->get('admin/stock', 'Admin::stock');
+
 
 // ---------- Emails ----------
 $routes->get('emails/order-confirmation', 'Emails::orderConfirmation');
--- images_php_sqlite/data/SMIYC/app/Views/accueil.php
+++ images_php_sqlite/data/SMIYC/app/Views/accueil.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- CSS -->
-    <link rel="stylesheet" href="../css/common.css">
-    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="Dev/css/common.css">
+    <link rel="stylesheet" href="Dev/css/index.css">
 
     <!-- FONTS -->
     <link rel="preconnect" href="https://fonts.googleapis.com">

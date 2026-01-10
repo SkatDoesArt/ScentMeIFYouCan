fe6098c55123ddbb18bd96e4aefb9cc97712270a
Yassar <ymamoufaka@gmail.com>
2026-01-10 03:22:16 +0100

    Sauvegarde avant pull

--- images_php_sqlite/data/SMIYC/app/Controllers/Cart.php
+++ images_php_sqlite/data/SMIYC/app/Controllers/Cart.php
@@ -2,22 +2,20 @@
 
 namespace App\Controllers;
 
-use App\Models\LignePanierModel;
+use App\Models\Panier\LignePanierModel;
 use App\Models\Panier\PanierModel;
 use Config\View;
 
 class Cart extends BaseController
 {
-      public function index(): string
-      {
-            $panierModel = new PanierModel();
-            $panier = $panierModel->getPanierComplet(1);
+    public function index(): string
+    {
+        $panierModel = new PanierModel();
+        $panier = $panierModel->getPanierComplet(1);
 
-            // La clé 'panier' sera le nom de la variable dans la vue
-            return view('Pages/panier/panier', ['panier' => $panier]);
-      }
-
-
+        // La clé 'panier' sera le nom de la variable dans la vue
+        return view('Pages/panier/panier', ['panier' => $panier]);
+    }
 
 
     // Augmente la quantité d'un produit

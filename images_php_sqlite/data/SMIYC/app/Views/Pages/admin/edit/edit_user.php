d71cdff0bca74b71ca70b698e83314e5916e22e9
Estevan <estevan.rolland@etu.univ-nantes.fr>
2026-01-12 17:42:47 +0100

    page produit améliorée

--- images_php_sqlite/data/SMIYC/app/Views/Pages/admin/edit/edit_user.php
+++ images_php_sqlite/data/SMIYC/app/Views/Pages/admin/edit/edit_user.php
@@ -1,6 +1,8 @@
 <?php
+?>
 <form action="<?= site_url('role/' . $user->id) ?>" method="post">
 <?= csrf_field() ?>
 <input type="hidden" name="statut" value="admin">
 <button type="submit">Passer admin</button>
 </form>
+

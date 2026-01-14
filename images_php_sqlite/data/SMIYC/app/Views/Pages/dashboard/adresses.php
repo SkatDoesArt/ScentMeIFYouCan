3427ed4333c046f06e351ff9b18da2a8e9e2a1c5
Mathis TONG--HATET <mathistonghatet@gmail.com>
2026-01-14 23:41:47 +0100

    restoration vues dashboard

--- images_php_sqlite/data/SMIYC/app/Views/Pages/dashboard/adresses.php
+++ images_php_sqlite/data/SMIYC/app/Views/Pages/dashboard/adresses.php
@@ -16,55 +16,58 @@
 <body>
     <?= view('Pages/partials/header', ['showCart' => true, 'showList' => false]) ?>
 
-    <div id="body">
-        <h1>Votre Compte</h1>
-        <div id="body-content">
-            <div id="categories">
-                <ul>
-                    <a href="<?= base_url(); ?>dashboard/infos_perso">
-                        <li>Informations personnelles</li>
-                    </a>
-                    <a href="<?= base_url(); ?>dashboard/langue_region">
-                        <li>Langue et Région</li>
-                    </a>
-                    <a href="<?= base_url(); ?>dashboard/adresses">
-                        <li><strong>Carnet d'adresses</strong></li>
-                    </a>
-                    <a href="<?= base_url(); ?>dashboard/moyen_paiement">
-                        <li>Informations de paiement</li>
-                    </a>
-                    <a href="<?= base_url(); ?>dashboard/suivi_commande">
-                        <li>Suivis de commande</li>
-                    </a>
-                    <a href="<?= base_url(); ?>dashboard/historique_commandes">
-                        <li>Historique de commandes</li>
-                    </a>
-                </ul>
-            </div>
-            <div id="infos">
-                <h2>Carnet d'adresses</h2>
-                <!-- <div id="list-adress">
+<div id="body">
+    <h1 class="dashboard-title">Votre Compte</h1>
+    <div id="body-content">
+        <div id="categories">
+            <ul>
+                <a href="<?= base_url(); ?>dashboard/infos_perso">
+                    <li>Informations personnelles</li>
+                </a>
+                <a href="<?= base_url(); ?>dashboard/langue_region">
+                    <li>Langue et Région</li>
+                </a>
+                <a href="<?= base_url(); ?>dashboard/adresses">
+                    <li><strong>Carnet d'adresses</strong></li>
+                </a>
+                <a href="<?= base_url(); ?>dashboard/moyen_paiement">
+                    <li>Informations de paiement</li>
+                </a>
+                <a href="<?= base_url(); ?>dashboard/suivi_commande">
+                    <li>Suivis de commande</li>
+                </a>
+                <a href="<?= base_url(); ?>dashboard/historique_commandes">
+                    <li>Historique de commandes</li>
+                </a>
+            </ul>
+        </div>
+        <div id="infos">
+            <h2>Carnet d'adresses</h2>
+            <!-- <div id="list-adress">
 
-                </div> -->
-                
-                <form action="">
-                    <div>
-                        <label for="destinataire">Destinataire :</label>
-                        <label for="adress">Adresse :</label>
-                        <label for="ville">Ville :</label>
-                        <label for="region">Région :</label>
-                        <label for="code-postal">Code Postal :</label>
-                        <label for="pays">Pays :</label>
-                    </div>
-                    <div>
-                        <input id="destinataire" name="destinataire" type="text">
-                        <input id="adress" name="adress" type="text">
-                        <input id="ville" name="ville" type="text">
-                        <input id="region" name="region" type="text">
-                        <input id="code-postal" name="code-postal" type="text">
-                        <input id="pays" name="pays" type="text">
-                    </div>
-                </form>
+            </div> -->
+
+            <form action="">
+                <div>
+                    <label for="destinataire">Destinataire :</label>
+                    <input id="destinataire" name="destinataire" type="text">
+
+                    <label for="adress">Adresse :</label>
+                    <input id="adress" name="adress" type="text">
+
+                    <label for="ville">Ville :</label>
+                    <input id="ville" name="ville" type="text">
+
+                    <label for="region">Région :</label>
+                    <input id="region" name="region" type="text">
+
+                    <label for="code-postal">Code Postal :</label>
+                    <input id="code-postal" name="code-postal" type="text">
+
+                    <label for="pays">Pays :</label>
+                    <input id="pays" name="pays" type="text">
+                </div>
+            </form>
 
                 <button id="modify-info">Modifier vos informations</button>
             </div>
--- images_php_sqlite/data/SMIYC/app/Views/Pages/dashboard/langue_region.php
+++ images_php_sqlite/data/SMIYC/app/Views/Pages/dashboard/langue_region.php
@@ -16,43 +16,43 @@
 <body>
     <?= view('Pages/partials/header', ['showCart' => true, 'showList' => false]) ?>
 
-    <div id="body">
-        <h1>Votre Compte</h1>
-        <div id="body-content">
-            <div id="categories">
-                <ul>
-                    <a href="<?= base_url(); ?>dashboard/infos_perso">
-                        <li>Informations personnelles</li>
-                    </a>
-                    <a href="<?= base_url(); ?>dashboard/langue_region">
-                        <li><strong>Langue et Région</strong></li>
-                    </a>
-                    <a href="<?= base_url(); ?>dashboard/adresses">
-                        <li>Carnet d'adresses</li>
-                    </a>
-                    <a href="<?= base_url(); ?>dashboard/moyen_paiement">
-                        <li>Informations de paiement</li>
-                    </a>
-                    <a href="<?= base_url(); ?>dashboard/suivi_commande">
-                        <li>Suivis de commande</li>
-                    </a>
-                    <a href="<?= base_url(); ?>dashboard/historique_commandes">
-                        <li>Historique de commandes</li>
-                    </a>
-                </ul>
-            </div>
-            <div id="infos">
-                <h2>Langue et Région</h2>
-                <form action="">
-                    <div>
-                        <label for="lang">Langue :</label>
-                        <label for="region">Région :</label>
-                    </div>
-                    <div>
-                        <input id="lang" name="lang" type="text" value="" readonly>
-                        <input id="region" name="region" type="text" readonly>
-                    </div>
-                </form>
+<div id="body">
+    <h1 class="dashboard-title">Votre Compte</h1>
+    <div id="body-content">
+        <div id="categories">
+            <ul>
+                <a href="<?= base_url(); ?>dashboard/infos_perso">
+                    <li>Informations personnelles</li>
+                </a>
+                <a href="<?= base_url(); ?>dashboard/langue_region">
+                    <li><strong>Langue et Région</strong></li>
+                </a>
+                <a href="<?= base_url(); ?>dashboard/adresses">
+                    <li>Carnet d'adresses</li>
+                </a>
+                <a href="<?= base_url(); ?>dashboard/moyen_paiement">
+                    <li>Informations de paiement</li>
+                </a>
+                <a href="<?= base_url(); ?>dashboard/suivi_commande">
+                    <li>Suivis de commande</li>
+                </a>
+                <a href="<?= base_url(); ?>dashboard/historique_commandes">
+                    <li>Historique de commandes</li>
+                </a>
+            </ul>
+        </div>
+        <div id="infos">
+            <h2>Langue et Région</h2>
+            <form action="">
+                <div>
+                    <label for="lang">Langue :</label>
+                    <input id="lang" name="lang" type="text" value="" readonly>
+
+                    <label for="region">Région :</label>
+                    <input id="region" name="region" type="text" readonly>
+
+                </div>
+            </form>
 
                 <button id="modify-info">Modifier vos informations</button>
             </div>
